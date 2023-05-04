8759f50f623db56b203cb779c8de6c343da4e217
jacobglz <jakojrglez@outlook.com>
2023-05-04 16:55:15 -0700

    Inicio del proyecto aplicacion movil de pedidos en restaurantes

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace InstantFood;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace InstantFood;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+
